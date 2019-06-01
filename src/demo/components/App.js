042340b5e53df4dbe95fceed774e22af53af7a2e
PRAKASH SUBEDI <psubedi2020@outlook.com>
2019-06-01 00:37:05 -0400

    implemented bubble chart to represent relative size of disasters caused by OWASP Ten as of 2017'

--- src/demo/components/App.js
+++ src/demo/components/App.js
@@ -7,8 +7,11 @@ module.exports = React.createClass ({
 		return (
 			<div>
 				<Nav />
-				<h1>React-d3-Library</h1>
+				<h1>Relative Size of disasters caused by OWASP Ten Volunerbilities, as of 2017</h1>
+				<h3>Visualization prepared by Prakash Subedi using React and D3 libraries.</h3> 
 				{this.props.children}
+				
+				<h3>Ref: https://snyk.io/blog/owasp-top-10-breaches/</h3>
 			</div>
 		)
 	}
--- src/demo/components/Nav.js
+++ src/demo/components/Nav.js
@@ -4,34 +4,9 @@ var {Link} = require('react-router');
 module.exports = React.createClass({
   render() {
     return (
-      <div>
+      <div align="center">
         <nav className="navbar">
-        	<li><Link to="/">Home</Link></li>
-          <li><Link to="/bubbleChart">Bubble Chart</Link></li>
-          <li><Link to="/binaryTree">Binary Tree</Link></li>
-          <li><Link to="/circleEventHandler">Circle Event</Link></li>
-          <li><Link to="/radialTree">Radial Tree</Link></li>
-          <li><Link to="/colorMesh">colorMesh</Link></li>
-          <li><Link to="/bestCanidate">Best Canidate</Link></li>
-          <li><Link to="/mergeSort">Merge Sort</Link></li>
-          <li><Link to="/roundedRectangles">Rounded Rectangles</Link></li>
-          <li><Link to="/congressionalDistricts">Congressional Districts</Link></li>
-          <li><Link to="/calendarView">Calendar View</Link></li>
-          <li><Link to="/barChartComponent">BarChart Component</Link></li>
-          <li><Link to="/pieChart">Pie Chart</Link></li>
-          <li><Link to="/scatterPlot">Scatter Plot</Link></li>
-          <li><Link to="/areaChart">Area Chart</Link></li>
-          <li><Link to="/lineChart">Line Chart</Link></li>
-          <li><Link to="/chainedTransitions">Chained Transitions</Link></li>
-          <li><Link to="/rainDrops">Rain Drops</Link></li>
-          <li><Link to="/particles">Particles</Link></li>
-          <li><Link to="/streamgraph">Streamgraph</Link></li>
-          <li><Link to="/interactiveStreamgraph">Interactive Streamgraph</Link></li>
-          <li><Link to="/dnaHelix">DNA Helix</Link></li>
-          <li><Link to="/stackedToBars">Stacked To Grouped Bars</Link></li>
-          <li><Link to="/growTree">Grow Tree</Link></li>
-          <li><Link to="/voronoiDiagram">Voronoi Diagram</Link></li>
-          <li><Link to="/mazeHover">Maze Hover</Link></li>
+          <Link to="/bubbleChart">Emergent Bubbles of Volunerbilities</Link>
         </nav>
       </div>
     )
